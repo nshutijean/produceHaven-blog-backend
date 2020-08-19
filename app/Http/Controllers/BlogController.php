bbd621190ba5a8268ec40fe3c36ddc7e9448a759
2nchy <j.nshuti73@gmail.com>
2020-08-19 11:49:37 +0200

    initial commit

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'username' => 'required',
+            'career' => 'required',
+            'socialMediaUrl' => 'required|url',
+            'title' => 'required',
+            'imageUrl' => 'required|url',
+            'content' => 'required'
+
+        ]);
+        $data = $request->only(['username', 'career', 'socialMediaUrl', 'title', 'imageUrl', 'content']);
+        $blogPost = Blog::create($data);
+
+        return response()->json([
+            'status' => (bool) $blogPost,
+            'data' => $blogPost,
+            'message' => $blogPost ? 'Blogpost created' : 'Error occured while createing post'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Blog $blog)
+    {
+        //
+    }
+}
